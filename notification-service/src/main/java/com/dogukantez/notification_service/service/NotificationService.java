e8e90e9dda0bb22521d1545943cb9d22261cc5d3
Doğukan Tez <dogukantez7@gmail.com>
2026-03-10 13:13:06 +0300

    feat:notification-service

---
+++ notification-service/src/main/java/com/dogukantez/notification_service/service/NotificationService.java
@@ -0,0 +1,17 @@
+package com.dogukantez.notification_service.service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class NotificationService {
+
+    @KafkaListener(topics = "order-placed",groupId = "notification-service")
+    public void getOrderNumber(String orderId){
+        log.info("Received order id: {}",orderId);
+
+    }
+}
